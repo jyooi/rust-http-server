6629e12b1c67d9367c5f41e6fba7c4ff148388e6
OOI JIA YI <ooijiayi93@gmail.com>
2023-11-14 13:27:11 +0800

    Pass 2nd stage : return HTTP 200 OK responsed ./your_server.sh

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Uncomment this block to pass the first stage
+use std::io::prelude::*;
 use std::net::TcpListener;
-
+use std::net::TcpStream;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -10,13 +11,26 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+      
+       
+        let stream = stream.unwrap();
+        handle_connection(stream)
+
     }
+
+}
+
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0,255];
+
+    stream.read(&mut buffer).unwrap();
+    
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..])); // print buffer
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+
+    stream.flush().unwrap();
 }
