0c636591eeb4960bcacc21bd227c796fb3b1e11b
OOI JIA YI <ooijiayi93@gmail.com>
2024-01-08 16:35:48 +0800

    stage 8

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,130 @@
-// Uncomment this block to pass the first stage
-
 use std::{
-    io::{BufRead, BufReader, Write, Read},
-    net::{TcpListener, TcpStream}, collections::HashMap,
+    env, fs,
+    fs::File,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    path::Path,
+    thread,
 };
-use std::env;
-use std::fs;
-use std::thread;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let mut directory = "default_directory";
-    for i in 0..args.len() {
-        if args[i] == "--directory" && i + 1 < args.len() {
-            directory = &args[i + 1];
-            break;
-        }
-    }
+    println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        let directory = directory.to_string();
-
-        thread::spawn(move || { // Spawn a new thread for each connection
-            handle_connection(stream, directory);
-        });
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_connection(stream);
+                });
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
     }
 }
-fn handle_connection(mut stream: TcpStream, directory: String) {
-    let mut reader = BufReader::new(&mut stream);
 
-    let mut request = String::new();
-    match reader.read_line(&mut request) {
-        Ok(bytes_read) => {
-            println!("{} bytes read", bytes_read);
-            let request_parts: Vec<&str> = request.split_whitespace().collect();
-            let http_path = request_parts.get(1).unwrap_or(&"").to_string();
-            let params = http_path.splitn(3, '/').last();
-            let request_lines: Vec<&str> = request.split('\n').collect();
-            let request_line = request_lines[0];
-            let parts: Vec<&str> = request_line.split_whitespace().collect();
-            let method = parts[0];
-           let body = match http_path.as_str() {
-            "/" =>  format!("HTTP/1.1 200 OK\r\n\r\n"),
-            path if path.starts_with("/echo/") => match params {
-                Some(params) => {                    
-                    let content = params.to_string();
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content.len(), content)
-                },
-                None => {
-                    String::from("HTTP/1.1 404 Not Found\r\n\r\n")
-                }
-            },
-            path if path.starts_with("/user-agent") => match params {
-                Some(_params) => {          
-                    let mut headers = HashMap::new();
-                        loop {
-                            let mut line = String::new();
-                            match reader.read_line(&mut line) {
-                                Ok(bytes_read) => {
-                                    if bytes_read == 0 || line == "\r\n" {
-                                        break;
-                                    }
-                                    let parts: Vec<&str> = line.splitn(2, ':').collect();
-                                    if parts.len() == 2 {
-                                        headers.insert(parts[0].trim().to_string(), parts[1].trim().to_string());
-                                    }
-                                },
-                                Err(error) => {
-                                    println!("error reading from stream: {}", error);
-                                    return;
-                                }
-                            };
-                        }
-                    let default_agent = "Unknown User-Agent".to_string();
-                    let user_agent = headers.get("User-Agent").unwrap_or(&default_agent);
-                    format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", user_agent.len(), user_agent)
-                },
-                None => {
-                    String::from("HTTP/1.1 404 Not Found\r\n\r\n")
-                }
-            },
-            path if path.starts_with("/files/") => {
-                match method {
-                    "POST" => {
-                        let filename = &path[7..];
-                        let filepath = format!("{}/{}", directory, filename);
-                        std::fs::create_dir_all(&directory).unwrap_or_else(|e| {
-                            println!("Failed to create directory: {}", e);
-                        });
+fn handle_connection(mut stream: TcpStream) {
+    let args: Vec<String> = env::args().collect();
+    let mut buf_reader = BufReader::new(&mut stream);
 
-                        match std::fs::File::create(&filepath) {
-                            Ok(mut file) => {
-                                let mut body = Vec::new();
-                                if let Err(e) = reader.read_to_end(&mut body) {
-                                    println!("Error reading from stream: {}", e);
-                                    return;
-                                }
+    let buffer = buf_reader.fill_buf().unwrap();
+    // println!("buffer: {:?}", buffer);
+    let result = String::from_utf8(buffer.to_vec()).unwrap();
 
-                                if let Err(e) = file.write_all(&body) {
-                                    println!("Error writing to file: {}", e);
-                                    return;
-                                }
+    // println!("result: {:?}", result);
+    // println!("buf_reader: {:?}", buf_reader);
+    let request_line: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    // println!("request_line: {:?}", request_line);
+    let user_agent_header = "User-Agent:";
+    let user_agent = request_line
+        .iter()
+        .find(|line| line.starts_with(user_agent_header))
+        .map(|line| line.trim_start_matches(user_agent_header).trim())
+        .unwrap_or_else(|| "Unknown User Agent");
 
-                                format!("HTTP/1.1 201 Created\r\n\r\n")
-                            },
-                            Err(e) => {
-                                println!("Error creating file: {}", e);
-                                format!("HTTP/1.1 500 Internal Server Error\r\n\r\n")
-                            }
-                        }
-                    },
-                    "GET" => {
-                        let filename = &path[7..];
-                        let filepath = format!("{}/{}", directory, filename);
-                        match fs::read(&filepath) {
-                            Ok(contents) => {
-                                format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}", contents.len(), String::from_utf8_lossy(&contents))
-                            },
-                            Err(_) => {
-                                String::from("HTTP/1.1 404 Not Found\r\n\r\n")
-                            }
-                        }
-                    },
-                    _ =>  format!("HTTP/1.1 405 Method Not Allowed\r\n\r\n"),
-                }
-            },
-            _ =>  format!("HTTP/1.1 404 Not Found\r\n\r\n"),
-        };
-        
-             match stream.write(body.as_bytes()) {
-                Ok(_bytes_written) => println!("HTTP 200 OK"),
-                Err(error) => {
-                    println!("error writing to stream: {}", error);
-                }
-            };
+    let split_line: Vec<_> = request_line[0].split(" ").collect();
+    let method = split_line[0];
+    let mut path = split_line[1].to_string();
+    if method == "GET" && path == "/" {
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+    if method == "GET" && path.starts_with("/echo/") {
+        path = path.replace("/echo", "");
+        if path.starts_with("/") {
+            path.remove(0);
         }
-        Err(error) => {
-            println!("error reading from stream: {}", error);
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            path.len(),
+            path
+        );
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if method == "GET" && path.starts_with("/user-agent") {
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            user_agent.len(),
+            user_agent
+        );
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if method == "GET" && path.starts_with("/files") {
+        let dir = args[2].to_string();
+        path = path.replace("/files", "");
+        if path.starts_with("/") {
+            path.remove(0);
         }
+        // println!("{} {}", path, filename);
+        let full_path = format!("{dir}/{path}");
+        if Path::new(&full_path).exists() {
+            let contents = fs::read_to_string(&full_path);
+            match contents {
+                Ok(contents) => {
+                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{contents}",
+                                    contents.len(),
+                                );
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
+                Err(..) => {
+                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    stream.write_all(response.as_bytes()).unwrap();
+                }
+            }
+        } else {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    } else if method == "POST" && path.starts_with("/files") {
+        let dir = args[2].to_string();
+        path = path.replace("/files", "");
+        if path.starts_with("/") {
+            path.remove(0);
+        }
+        println!("{}", path);
+        // println!("{} {}", path, filename);
+        let full_path = format!("{dir}/{path}");
+        let body_header = "\r\n\r\n";
+        let body: String = result
+            .splitn(2, body_header)
+            .nth(1)
+            .map(|s| s.to_string())
+            .unwrap_or_else(|| "No Body".to_string());
+        if body == "No Body" {
+            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            stream.write_all(response.as_bytes()).unwrap();
+        } else {
+            let mut file = File::create(full_path).unwrap();
+            file.write_all(body.as_bytes()).unwrap();
+            let response = format!("HTTP/1.1 201 OK \r\n\r\n");
+            stream.write_all(response.as_bytes()).unwrap();
+        }
+    } else {
+        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
